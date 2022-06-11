56ac687b341ad35d3b077e5c99b0df4077cc2468
Mohamed Jamir <mohamedjamir@mohameds-air.lan>
2022-06-11 14:39:08 +0800

    new

--- script.js
+++ script.js
@@ -2,11 +2,16 @@ const quoteContainer = document.getElementById("quote-container");
 const quoteTextA = document.getElementById("quotea");
 const quoteTextE = document.getElementById("quotee");
 const ayahText = document.getElementById("ayah");
-const twitterBtn = document.getElementById("twitter");
+const teleBtn = document.getElementById("tele");
 const newQuoteBtn = document.getElementById("new-quote");
 const loader = document.getElementById("loader");
 
+
 let apiQuote = [];
+
+let url = new URL("https://ayahgen-lyart.vercel.app/");
+
+
 
 //show new quote
 
@@ -24,7 +29,16 @@ function complete() {
 
 async function newQuote() {
   loading();
-  verse = Math.floor(Math.random() * 6237) + 1;
+
+  const query = window.location.search;
+  const urlParams = new URLSearchParams(query);
+  let verse = 0;
+  if (urlParams.get("v") == null) {
+    verse = Math.floor(Math.random() * 6237) + 1;
+    console.log(toString(verse))
+  } else {
+    verse = urlParams.get("v");
+  }
   const apiurl =
     "https://api.alquran.cloud/ayah/" +
     verse +
@@ -65,66 +79,23 @@ ayahText.textContent =
     ")" +
     ":" +
     apiQuote["data"][0]["numberInSurah"];
-
+    url.searchParams.append("v",verse)
+  window.location.href = url;
   complete();
+
+ 
 }
 
-//get ayah from api
-async function getayah() {
-  loading();
-  let verse = Math.floor(Math.random() * 6237) + 1;
-  const apiurl =
-    "https://api.alquran.cloud/ayah/" +
-    verse +
-    "/editions/quran-uthmani,en.pickthall";
-
-  try {
-    let time1 = performance.now(); //! Time Start
-    const response = await fetch(apiurl);
-    apiQuote = await response.json();
-    let time2 = performance.now(); //! Time End
-
-    console.log("getAyah() performance :")
-    console.log(time2-time1) //! Time taken to execute
-    console.log(apiQuote);
-  } catch (error) {
-    //catch error
-  }
-
-  arabic = apiQuote["data"][1]["text"];
-  english = apiQuote["data"][0]["text"];
-
-  if (english.length > 50) {
-    quoteTextA.classList.add("long-quote");
-    quoteTextE.classList.add("long-quote");
-    console.log("long");
-  } else {
-    quoteTextA.classList.remove("long-quote");
-    quoteTextE.classList.remove("long-quote");
-  }
-
-  quoteTextA.textContent = arabic;
-  quoteTextE.textContent = english;
-
-  ayahText.textContent =
-    apiQuote["data"][0]["surah"]["englishName"] +
-    "(" +
-    apiQuote["data"][0]["surah"]["number"] +
-    ")" +
-    ":" +
-    apiQuote["data"][0]["numberInSurah"];
-
-  complete();
-}
 
 function telegram() {
-  const teleurl = `https://t.me/share/url?url=${pageurl}&text=${message}`;
+  const teleurl = `https://t.me/share/url?url=${"google.com"}&text=${"message"}`;
   window.open(teleurl, "_blank");
 }
 
 //Eventlistner
 
-newQuoteBtn.addEventListener("click", getayah);
-twitterBtn.addEventListener("click", telegram);
+newQuoteBtn.addEventListener("click", newQuote);
+teleBtn.addEventListener("click", telegram);
 
-getayah();
+newQuote();
+
